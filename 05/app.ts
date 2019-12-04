03acbf8c66b2d17658f8324bba886852bea6a232
WouterMartens <54664359+WouterMartens@users.noreply.github.com>
2019-12-04 17:21:30 +0100

    xd

--- 05/app.ts
+++ 05/app.ts
@@ -1,15 +1,37 @@
 const start: number = 240298;
-// const end: number = 784956;
-const end: number = 250000;
+const end: number = 784956;
+// const end: number = 250000;
+
+const uniques: Set<number> = new Set();
 
 for (let i = start; i <= end; i++) {
-    const arr: string[] = String(i).split('');
-    // console.log(arr);
-    
-    for (let c = 0; c < arr.length; c++) {
-        if (arr[c] <= arr[c - 1] && c !== 0) {
-            arr[c] = arr[c - 1];
-            console.log(arr[c]);
-        }       
+    if (correctNumber(i)) {
+        uniques.add(i);
     }
 }
+
+function correctNumber(value: number): boolean {
+    const arr: number[] = String(value).split('').map(Number);
+    let doubleCheck: boolean = false;
+    let increasing: boolean = true;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i - 1] === arr[i] || arr[i + 1] === arr[i]) {
+            doubleCheck = true;
+        }
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i !== 0 && arr[i] < arr[i - 1]) {
+            increasing = false;
+            break;
+        }
+    }
+
+    if (doubleCheck && increasing) {
+        return true;
+    }
+    return false;
+}
+
+console.log(uniques);
